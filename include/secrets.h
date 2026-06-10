c585d987dc72a6a4ea93d3d0b58353209735ece5
endresenerik-hash <erikendresen@gmail.com>
2026-06-10 19:33:01 +0200

    twest2

--- include/secrets.h
+++ include/secrets.h
@@ -2,13 +2,13 @@
 #pragma once
 
 // This defines the name of your device
-#define WIFI_HOSTNAME "ikea-led2"
+#define WIFI_HOSTNAME "Ikea-Veggklokke"
 
-#ifdef ESP8266
-#define WIFI_SSID ""
-#define WIFI_PASSWORD ""
+#ifdef ESP32dev
+#define WIFI_SSID "Wu-Tang-Lan"
+#define WIFI_PASSWORD "Vajayjay6853!"
 #endif
 
 // If you would like to perform OTA updates, you need to define the credentials here
-#define OTA_USERNAME "admin"
-#define OTA_PASSWORD "ikea-led-wall"
+#define OTA_USERNAME "erik"
+#define OTA_PASSWORD "Banan23"
